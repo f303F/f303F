84dc38125a82c7d5a1a6c9de67bd171d224a665f
f303F <good91207338@gmail.com>
2024-08-24 21:26:43 +0400

    la3w.js

---
+++ la3w.js
@@ -0,0 +1,36 @@
+[
+
+    {
+        "img": "https://telegra.ph/file/4f58e51e1ffab030d21d9.jpg",
+        "jawaban": "يوها باخ"
+        },
+    {   "img": "https://telegra.ph/file/0a0ec73f796ac2f4ef4c3.jpg",
+        "jawaban": "ثورفين"
+        },
+    {
+        "img": "https://telegra.ph/file/1d74c3ba5bf94ab152c7a.jpg",
+        "jawaban": "روكيا"
+        },
+    {
+        "img": "https://telegra.ph/file/05816c7b6e7ee0d540cd4.jpg",
+        "jawaban": "سايتاما"
+        },
+    {
+        "img": "https://telegra.ph/file/b686ad42217d92e2f07d6.jpg",
+        "jawaban": "ياماتو"
+        },
+    {
+        "img": "https://telegra.ph/file/33cc2228ff4e206bd4847.jpg",
+        "jawaban": "نامي"
+        },
+    {
+        "img": "https://telegra.ph/file/a935e6e41c1326ad1aa36.jpg",
+        "jawaban": "غيو"
+        },
+    {
+        "img": "https://telegra.ph/file/102680c38aac9394d4bc6.jpg",
+        "jawaban": "يور"
+        },
+    {
+        }
+]
