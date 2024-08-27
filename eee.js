6725659f455dff63157aed1af5685d69b8c225ea
f303F <good91207338@gmail.com>
2024-08-27 21:31:21 +0400

    Update eee.js

--- eee.js
+++ eee.js
@@ -2,13 +2,13 @@
   {
     "index": 0,
     "img": "https://www.cademedia.com/wp-content/uploads/2020/12/tebak-gambar-level-1-nomor-1.jpg",
-    "jawaban": "TANTANGAN SERU",
+    "jawaban": "تيت SERU",
     "deskripsi": "Gambar tang (huruf G dicoret), tangan dan tanda seru (pentung)."
   },
   {
     "index": 1,
     "img": "https://www.cademedia.com/wp-content/uploads/2020/12/tebak-gambar-level-1-nomor-2.jpg",
-    "jawaban": "TENAGA LISTRIK",
+    "jawaban": "وظنظ LISTRIK",
     "deskripsi": "Gambar huruf TE, ular naga dan tegangan listrik (petir)."
   }
  ]
