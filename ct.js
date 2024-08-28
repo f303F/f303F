487295e909f8969a7729c46c208a4385e34ace3c
f303F <good91207338@gmail.com>
2024-08-28 15:27:52 +0400

    Create ct.js

---
+++ ct.js
@@ -0,0 +1,36 @@
+"[
+    {
+        "soal": "لاو": [
+            "sheraton",
+            "the westin",
+            "kempinski",
+            "the langham",
+            "borobudur",
+            "hilton"
+        ]
+    },
+    {
+        "soal": "تيتش",
+        "jawaban": [
+            "pensil",
+            "botol minum",
+            "pensil warna",
+            "sapu tangan",
+            "hapusan",
+            "buku"
+        ]
+    },
+    {
+        "soal": "بيبو",
+        "jawaban": [
+            "ulang tahun",
+            "pesta",
+            "pernikahan",
+            "piknik",
+            "wisuda",
+            "reuni",
+            "arisan",
+            "gathering"
+        ]
+    },
+    {
