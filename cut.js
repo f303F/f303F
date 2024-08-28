b013b936b5cf05b842063c246ff710fe774c7d61
f303F <good91207338@gmail.com>
2024-08-28 21:33:46 +0400

    Update cut.js

--- cut.js
+++ cut.js
@@ -1,15 +1,166 @@
 [
   {
     "index": 0,
-    "soal": "لاو, , , , ",
+    "soal": "لاو",
     "jawaban": "لاو"
   },
   {
     "index": 1,
-    "soal": "نامي",
-    "jawaban": "نامي"
+    "soal": "مادارا",
+    "jawaban": "مادارا"
   },
   {
+    "index": 1,
+    "soal": "كاكاشي",
+    "jawaban": "كاكاشي"
+  },
+  {
+    "index": 1,
+    "soal": "غارو",
+    "jawaban": "غارو"
+  },
+  {
+    "index": 1,
+    "soal": "شينوبو",
+    "jawaban": "شينوبو"
+  },
+  {
+    "index": 1,
+    "soal": "توكيتو",
+    "jawaban": "توكيتو"
+  },
+  {
+    "index": 1,
+    "soal": "هيسوكا",
+    "jawaban": "هيسوكا"
+  },
+  {
+    "index": 1,
+    "soal": "ميهوك",
+    "jawaban": "ميهوك"
+  },
+  {
+    "index": 1,
+    "soal": "كروغر",
+    "jawaban": "كروغر"
+  },
+  {
+    "index": 1,
+    "soal": "يوها باخ",
+    "jawaban": "يوها باخ"
+  },
+  {
+    "index": 1,
+    "soal": "ايتشيغو",
+    "jawaban": "ايتشيغو"
+  },
+  {
+    "index": 1,
+    "soal": "سوكونا",
+    "jawaban": "سوكونا"
+  },
+  {
+    "index": 1,
+    "soal": "نانامي",
+    "jawaban": "نانامي"
+  },
+  {
+    "index": 1,
+    "soal": "غوجو",
+    "jawaban": "غوجو"
+  },
+  {
+    "index": 1,
+    "soal": "ايرزا",
+    "jawaban": "ايرزا"
+  },
+  {
+    "index": 1,
+    "soal": "ليفاي",
+    "jawaban": "ليفاي"
+  },
+  {
+    "index": 1,
+    "soal": "سابيتو",
+    "jawaban": "سابيتو"
+  },
+  {
+    "index": 1,
+    "soal": "سابو",
+    "jawaban": "سابو"
+  },
+  {
+    "index": 1,
+    "soal": "غيو",
+    "jawaban": "غيو"
+  },
+  {
+"index": 1,
+    "soal": "روبين",
+    "jawaban": "روبين"
+  },
+  {
+    "index": 1,
+    "soal": "ياماتو",
+    "jawaban": "ياماتو"
+  },
+  {
+    "index": 1,
+    "soal": "يوتا",
+    "jawaban": "يوتا"
+  },
+  {
+    "index": 1,
+    "soal": "ايرين",
+    "jawaban": "ايرين"
+  },
+  {
+    "index": 1,
+    "soal": "ميكاسا",
+    "jawaban": "ميكاسا"
+  },
+  {
+    "index": 1,
+    "soal": "بيبو",
+    "jawaban": "بيبو"
+  },
+  {
+    "index": 1,
+    "soal": "غوكو",
+    "jawaban": "غوكو"
+  },
+  {
+    "index": 1,
+    "soal": "ناروتو",
+    "jawaban": "ناروتو"
+  },
+  {
+    "index": 1,
+    "soal": "ساسكي",
+    "jawaban": "ساسكي"
+  },
+  {
+    "index": 1,
+    "soal": "لوفي",
+    "jawaban": "لوفي"
+  },
+  {
+    "index": 1,
+    "soal": "زورو",
+    "jawaban": "زورو"
+  },
+  {
+    "index": 1,
+    "soal": "سيرا",
+    "jawaban": "سيرا"
+  },
+  {
+    "index": 1,
+    "soal": "كونان",
+    "jawaban": "كونان"
+  },
+  {
+    
     "index": 2,
     "soal": "شادو",
     "jawaban": "شادو"
