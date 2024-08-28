14233670e199cba6f421aa18b66dfb148eeeb76a
f303F <good91207338@gmail.com>
2024-08-28 15:47:30 +0400

    Update ct.js

--- ct.js
+++ ct.js
@@ -2,36 +2,19 @@
     {
         "soal": "تيتش",
         "jawaban": [
-            "sheraton",
-            "the westin",
-            "kempinski",
-            "the langham",
-            "borobudur",
-            "hilton"
+            "تيتش",
         ]
     },
     {
         "soal": "نامي",
         "jawaban": [
             "نامي",
-            "botol minum",
-            "pensil warna",
-            "sapu tangan",
-            "hapusan",
-            "buku"
         ]
     },
     {
         "soal": "لاو",
         "jawaban": [
             "لاو",
-            "تراو",
-            "pernikahan",
-            "piknik",
-            "wisuda",
-            "reuni",
-            "arisan",
-            "gathering"
         ]
     }
 ]
